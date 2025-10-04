9bf8f0586e650c20da206be20a91ec2d530bde0c
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-10-04 13:20:08 -0300

    New field: MONGO_URI

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,5 +1,10 @@
 import 'dotenv/config';
 
-export const config = {
-    port: process.env.PORT || 5000,
+export const PORT = process.env.PORT || 5000
+
+export const MONGO_URI = process.env.MONGO_URI
+
+export default {
+    PORT,
+    MONGO_URI,
 };
