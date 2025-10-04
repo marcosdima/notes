a347bed0ff9f4b7b0d83b0a390ec0bf97b01c333
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-10-04 16:37:51 -0300

    next unused lint warning

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -2,7 +2,7 @@ export const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' });
 };
 
-export const errorHandler = (err, req, res, next) => {
+export const errorHandler = (err, req, res) => {
     console.error(err);
     res.status(err.status || 500).json({
         error: err.message || 'Internal Server Error',
