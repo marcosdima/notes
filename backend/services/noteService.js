7a6ef20915dfb99d4e0a54806cccd1d7cb14ef64
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-10-05 13:43:27 -0300

    Service for notes

---
+++ backend/services/noteService.js
@@ -0,0 +1,28 @@
+import { Note } from '../models/note.js';
+
+class NoteService {
+    maxContentLenght = Note.noteLimits().contentMax;
+    maxTitleLenght = Note.noteLimits().titleMax;
+
+    async getAll() {
+        return await Note.find();
+    }
+
+    async getById(id) {
+        return await Note.findById(id);
+    }
+
+    async create(data) {
+        return await Note.create(data);
+    }
+
+    async update(id, data) {
+        return await Note.findByIdAndUpdate(id, data, { new: true });
+    }
+
+    async remove(id) {
+        return await Note.findByIdAndDelete(id);
+    }
+};
+
+export const noteService = new NoteService()
