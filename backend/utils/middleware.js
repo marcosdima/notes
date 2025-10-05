73fdaed202e87dc959c98f20b5b80587bd45c642
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-10-05 13:44:01 -0300

    update middleware

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -1,9 +1,11 @@
-export const unknownEndpoint = (request, response) => {
+import { error } from './logger.js';
+
+export const unknownEndpoint = (req, res) => {
     response.status(404).send({ error: 'unknown endpoint' });
 };
 
-export const errorHandler = (err, req, res) => {
-    console.error(err);
+export const errorHandler = (err, req, res, next) => {
+    error(`\nError: ${err.status} - Message: ${err.message}\n`);
     res.status(err.status || 500).json({
         error: err.message || 'Internal Server Error',
     });
