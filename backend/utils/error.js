16331dd51a1b3b2d3d5dea907e4893e365c1449b
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-10-05 13:26:35 -0300

    Custom errors

---
+++ backend/utils/error.js
@@ -0,0 +1,30 @@
+export class AppError extends Error {
+    constructor(message, status = 500) {
+        super(message);
+        this.status = status;
+        this.name = 'AppError';
+    }
+}
+
+export class FieldsError extends AppError {
+    constructor(message, fields) {
+        super(message + fields.join(', ') + '.', 400);
+        this.name = 'FieldsError';
+    }
+}
+
+export class MissingFields extends FieldsError {
+    constructor(fields = []) {
+        const end = (fields.length <= 1) ? ': ' : 's: ';
+        super('Missing field' + end, fields);
+        this.name = 'MissingFields';
+    }
+}
+
+export class InvalidFields extends FieldsError {
+    constructor(fields = []) {
+        const end = 's: ' ? fields.length <= 1 : ': ';
+        super('Invalid field' + end, fields);
+        this.name = 'InvalidFields';
+    }
+}
