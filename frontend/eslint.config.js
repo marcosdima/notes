24421f2e00792f6712c1891b178ad3ff77d79777
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-10-06 09:35:47 -0300

    Project template

---
+++ frontend/eslint.config.js
@@ -0,0 +1,34 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default defineConfig([
+    globalIgnores(['dist', 'node_modules']),
+    {
+        files: ['**/*.{js,jsx}'],
+        extends: [
+            js.configs.recommended,
+            reactHooks.configs['recommended-latest'],
+            reactRefresh.configs.vite,
+        ],
+        languageOptions: {
+            globals: globals.browser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                ecmaFeatures: { jsx: true },
+                sourceType: 'module',
+            },
+        },
+        rules: {
+            'no-unused-vars': ['warn', { varsIgnorePattern: '^[A-Z_]' }],
+            'no-console': 'off',
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn',
+            'quotes': ['warn', 'single', { avoidEscape: true }],
+            'semi': ['error', 'always'],
+            'indent': ['warn', 4],
+        },
+    },
+]);
