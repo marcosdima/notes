68f67da7a5aac033544ac19ea37c672b6280ee10
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-10-06 12:09:44 -0300

    Setting theme and router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
+import { Link, Outlet } from 'react-router-dom';
+import SideBar from './components/SideBar';
+
 function App() {
     return (
-        <>
-            App
-        </>
+        <div>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/notes/a">Note</Link>
+                <Link to="/notes/create/a">Create Note</Link>
+            </nav>
+            <Outlet />
+        </div>
     );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme.js';
+import { GlobalStyle } from './styles/global.js';
+import router from './router.jsx';
+import { RouterProvider } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <RouterProvider router={router} />
+        </ThemeProvider>
     </StrictMode>,
 );
