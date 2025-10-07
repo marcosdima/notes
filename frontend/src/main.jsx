e8224a2e83b92e9c11acbcef135e1e266ea87508
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-10-07 09:43:10 -0300

    provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,16 @@ import { theme } from './styles/theme.js';
 import { GlobalStyle } from './styles/global.js';
 import router from './router.jsx';
 import { RouterProvider } from 'react-router-dom';
+import { store } from './store/index.js';
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <ThemeProvider theme={theme}>
-            <GlobalStyle />
-            <RouterProvider router={router} />
-        </ThemeProvider>
+        <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                <GlobalStyle />
+                <RouterProvider router={router} />
+            </ThemeProvider>
+        </Provider>
     </StrictMode>,
 );
