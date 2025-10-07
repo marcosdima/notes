7f04aaef888e8ff760a329cbdeefbea90455aa20
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-10-07 09:38:19 -0300

    unknown endpoint message

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -1,9 +1,10 @@
 import { error } from './logger.js';
 
 export const unknownEndpoint = (req, res) => {
-    res.status(404).send({ error: 'unknown endpoint' });
+    res.status(404).send({ error: `unknown endpoint ${req.originalUrl}` });
 };
 
+// eslint-disable-next-line no-unused-vars
 export const errorHandler = (err, req, res, next) => {
     error(`\nError: ${err.status} - Message: ${err.message}\n`);
     res.status(err.status || 500).json({
