71616b4595a311e09f02f687089fbbb86f423fa4
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-10-07 09:42:13 -0300

    sub title style

---
+++ frontend/src/styles/elements/SubTitle.style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export default styled.div`
+    font-weight: 300;
+    font-size: ${({ theme }) => theme.font.size.xl};
+    color: ${({ theme }) => theme.colors.text.color};
+    text-align: left;
+    letter-spacing: 0.5px;
+    cursor: default;
+    transition: color 0.3s ease;
+
+    &:hover {
+        color: ${({ theme }) => theme.colors.text.hover};
+    }
+`;
