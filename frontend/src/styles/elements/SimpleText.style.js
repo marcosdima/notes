0ac4a24674f0944c9c425967406ae51ad44d26b2
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-10-07 09:39:52 -0300

    new styles

---
+++ frontend/src/styles/elements/SimpleText.style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export default styled.div`
+    font-size: 0.9rem;
+    color: ${({ theme }) => theme.colors.text.color};
+    opacity: 0.7;
+`; 
--- frontend/src/styles/elements/Title.style.js
+++ frontend/src/styles/elements/Title.style.js
@@ -1,14 +1,12 @@
 import styled from 'styled-components';
 
-export default styled.h1`
+export default styled.div`
     font-weight: 500;
     font-size: ${({ theme }) => theme.font.size.title};
     color: ${({ theme }) => theme.colors.text.color};
     text-align: left;
-    margin: 1rem;
     letter-spacing: 0.5px;
     cursor: default;
-    user-select: none;
     transition: color 0.3s ease;
 
     &:hover {
