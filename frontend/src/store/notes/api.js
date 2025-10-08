148680b64e30ee694f9e0dc48e9a8f69f75190b6
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-10-08 13:57:52 -0300

    createNote api

--- frontend/src/store/notes/api.js
+++ frontend/src/store/notes/api.js
@@ -6,6 +6,17 @@ export const getNotes = async () => {
         const { data } = await axios.get(apiRoutes.getNotes);
         return data;
     } catch (err) {
-        console.error(err);
+        handleError(err);
     }
 };
+
+export const createNote = async (noteData) => {
+    try {
+        const { data } = await axios.post(apiRoutes.createNotes, noteData);
+        return data;
+    } catch (err) {
+        handleError(err);
+    }
+};
+
+const handleError = (err) => { throw new Error(err.response.data.error); };
