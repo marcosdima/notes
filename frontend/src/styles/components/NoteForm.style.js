219e3a65439b638adacf77a0ca61be1c6418a4c2
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-10-09 15:24:43 -0300

    mod style

--- frontend/src/styles/components/NoteForm.style.js
+++ frontend/src/styles/components/NoteForm.style.js
@@ -3,15 +3,25 @@ import TitleStyle from '../elements/Title.style';
 
 export const NoteFormTitle = styled(TitleStyle)`
     text-align: center;
+    padding-top: 2rem;
+    padding-left: 2rem;
+`;
+
+export const NoteFormBottom = styled.div`
+    padding-top: 1rem;
+    padding-left: 5rem;
+    padding-right: 5rem;
+    display: flex;
+    gap: 2rem;
+    flex-direction: column;
+`;
+
+export const NoteFormUp = styled.div`
+    border-bottom: ${({ theme }) => theme.border.small };
 `;
 
 export default styled.div`
     width: 100%;
-    padding: 3rem;
-    display: flex;
-    gap: 1rem;
-    flex-direction: column;
-    align-items: left;
     background: ${({ theme }) => theme.colors.primary };
     overflow: auto;
 `;
