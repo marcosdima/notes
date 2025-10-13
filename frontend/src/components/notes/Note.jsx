32b026e0e2f0d0d5afe436513d3114e5f5619a20
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-10-13 09:25:06 -0300

    Note data

--- frontend/src/components/notes/Note.jsx
+++ frontend/src/components/notes/Note.jsx
@@ -1,6 +1,18 @@
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
 const Note = () => {
+    const { notes } = useSelector((state) => state.notes);
+    const { id } = useParams();
+
+    // If does not exists a note with the id provided...
+    const note = notes.find(({ _id }) => _id === id);
+    if (!note) return <></>;
+
+    const { title, content } = note;
+
     return (
-        <>Note</>
+        <>Note: {title} - Content: {content}</>
     );
 };
 
