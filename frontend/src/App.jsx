55397191c3adcbee4235771ca8c1d70f978f7d62
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-10-14 14:45:30 -0300

    fetch notes at App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 import { motion } from 'motion/react';
 import SideBar from './components/layout/side-bar';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchNotes } from './store/notes/thunks.js';
 
 const AppStyle = styled(motion.div)`
     background: ${({theme}) => theme.colors.background};
@@ -11,6 +14,12 @@ const AppStyle = styled(motion.div)`
 `;
 
 function App() {
+    const dispatch = useDispatch();
+    
+    useEffect(() => {
+        dispatch(fetchNotes());
+    }, [dispatch]);
+
     return (
         <AppStyle>
             <SideBar/>
