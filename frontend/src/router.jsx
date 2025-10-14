3bf967d7467ad398a3f01e3c2edcc5a546a5815e
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-10-14 10:37:00 -0300

    Home route

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,12 +1,26 @@
 // router.jsx
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
+import { appRoutes } from './utils/routes';
+import Home from './components/layout/Home';
+import Notes from './components/notes';
 
 const routes = [
     {
         path: '/',
-        element: <App />,
-        children: [],
+        element: <App/>,
+        children: [
+            {
+                path: appRoutes.home,
+                element: <Home/>,
+                children: [
+                    {
+                        path: appRoutes.notes,
+                        element: <Notes/>
+                    },
+                ],
+            },
+        ],
     },
 ];
 
