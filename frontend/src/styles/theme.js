66af197ddb6a571658c31981001165edb53483d2
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-10-14 15:18:37 -0300

    font sizes clamped

--- frontend/src/styles/theme.js
+++ frontend/src/styles/theme.js
@@ -11,11 +11,11 @@ export const theme = {
     },
     font: {
         size: {
-            small: '0.875rem',
-            base: '1rem',
-            large: '1.25rem',
-            xl: 'clamp(0.5rem, 1vh + 1vw + 1rem, 2rem)',
-            title: 'clamp(0.5rem, 2vh + 2vw + 2rem, 5rem)',
+            small: 'clamp(0.5rem, 0.4vw + 0.4vh + 0.5rem, 0.875rem)',
+            base: 'clamp(0.75rem, 0.5vw + 0.5vh + 0.5rem, 1rem)',
+            large: 'clamp(1rem, 0.8vw + 0.8vh + 0.8rem, 1.25rem)',
+            xl: 'clamp(1.25rem, 1vh + 1vw + 1rem, 2rem)',
+            title: 'clamp(1.5rem, 2vh + 2vw + 1rem, 4rem)',
         },
         family: 'Inter, sans-serif',
     },
