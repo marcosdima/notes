c96e042e66c50a86ab7a77565175328589edcce7
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-10-13 22:53:26 -0300

    color and font-family

--- frontend/src/styles/global.style.js
+++ frontend/src/styles/global.style.js
@@ -4,6 +4,8 @@ const GlobalStyle = createGlobalStyle`
     body {
         margin: 0;
         padding: 0;
+        color: ${({ theme }) => theme.colors.text.color};
+        font-family: ${({ theme }) => theme.font.family};
     }
 `;
 
