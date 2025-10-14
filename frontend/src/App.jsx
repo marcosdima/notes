c007f55d3edb1610c2259c4abd028418123dcd69
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-10-14 10:21:38 -0300

    App style in file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import { Outlet } from 'react-router-dom';
-import AppStyle from './styles/components/App.style';
+import styled from 'styled-components';
+import { motion } from 'motion/react';
+import SideBar from './components/layout/side-bar';
+
+const AppStyle = styled(motion.div)`
+    background: ${({theme}) => theme.colors.background};
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+`;
 
 function App() {
     return (
         <AppStyle>
-            <Outlet />
+            <SideBar/>
+            <Outlet/>
         </AppStyle>
     );
 }
